cef98a798349ba3917ea3fb3a6d8e08ec7001726
tkong9 <taewon2711@gmail.com>
2020-03-22 15:01:52 -0400

    컴포넌트의 라이프사이클 메서트

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -8,11 +8,34 @@ import './App.css';
 // import ValidationSample from './ValidationSample';
 // import ScrollBox from './ScrollBox';
 import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 // class compoment
 class App extends Component {
+  state = {
+    color: '#000000'
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  };
+
   render() {
-    return <IterationSample></IterationSample>;
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color}></LifeCycleSample>
+        </ErrorBoundary>
+      </div>
+    );
   }
 }
 
