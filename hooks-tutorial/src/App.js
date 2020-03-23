142a184117a6565a5b6f046e1026f595e3af92f6
tkong9 <taewon2711@gmail.com>
2020-03-23 19:08:43 -0400

    Hooks

---
+++ hooks-tutorial/src/App.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Counter from "./Counter";
+import Info from "./Info";
+import Average from "./Average";
+
+function App() {
+  const [visible, setVisible] = useState(false);
+  return <Average></Average>;
+}
+
+export default App;
